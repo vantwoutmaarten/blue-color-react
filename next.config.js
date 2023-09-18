c578d570ea1629835128207a5a6c58f655aa96f5
vantwoutmaarten <maarten@vantwout.net>
2023-09-18 21:41:47 +0200

    fix: commit build and deploy workflow and include github actions in nextjsconfig.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: "/blue-color-react",
-  assetPrefix: "/blue-color-react/",
-  images: {
-    unoptimized: true,
-  },
-};
+// next.config.js
 
-module.exports = nextConfig;
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
