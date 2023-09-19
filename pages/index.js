77a3484c8b8b9903b4694ada02eaaf169f884f11
vantwoutmaarten <maarten@vantwout.net>
2023-09-19 21:07:18 +0200

    fix: passing a setState select function as a props and then using it within a child component. And then displaying an object in another component after an item has been selected.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 
@@ -6,10 +7,11 @@ import PropTypes from "prop-types";
 import styles from "../styles/Home.module.css";
 import pokemon from "./pokemon.json";
 
-const PokemonRow = ({ pokemon }) => (
+const PokemonRow = ({ pokemon, onSelect }) => (
   <tr key={pokemon.id}>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(", ")}</td>
+    <button onClick={() => onSelect(pokemon)}>Select!</button>
   </tr>
 );
 
@@ -19,10 +21,42 @@ PokemonRow.propTypes = {
       english: PropTypes.string,
     }),
     type: PropTypes.arrayOf(PropTypes.string),
+    onSelect: PropTypes.func,
+  }),
+};
+
+const PokemonInfo = ({ name, base }) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      {Object.keys(base).map((key) => (
+        <tr key={key}>
+          <td>{key}</td>
+          <td>{base[key]}</td>
+        </tr>
+      ))}
+    </table>
+  </div>
+);
+
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({
+    english: PropTypes.string,
+  }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defense": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired,
   }),
 };
 
 export default function Home() {
+  const [filter, filterSet] = React.useState("");
+  const [selectedItem, selectedItemSet] = React.useState("");
+
   return (
     <div
       style={{
@@ -32,19 +66,46 @@ export default function Home() {
       }}
     >
       <h1 className={styles.title}>Pokemon Search</h1>
-      <table width="100%">
-        <thead>
-          <tr>
-            <th className={styles.propertyName}>Name</th>
-            <th className={styles.propertyName}>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pokemon.slice(0, 20).map((pokemon) => (
-            <PokemonRow pokemon={pokemon} key={pokemon.id} />
-          ))}
-        </tbody>
-      </table>
+      <input
+        className={styles.search}
+        value={filter}
+        onChange={(event) => filterSet(event.target.value)}
+      />
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "70% 30%",
+          gridColumnGap: "1rem",
+        }}
+      >
+        <div>
+          <table width="100%">
+            <thead>
+              <tr>
+                <th className={styles.propertyName}>Name</th>
+                <th className={styles.propertyName}>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon
+                .filter((pokemon) =>
+                  pokemon.name.english
+                    .toLowerCase()
+                    .includes(filter.toLowerCase())
+                )
+                .slice(0, 20)
+                .map((pokemon) => (
+                  <PokemonRow
+                    pokemon={pokemon}
+                    key={pokemon.id}
+                    onSelect={(pokemon) => selectedItemSet(pokemon)}
+                  />
+                ))}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && <PokemonInfo {...selectedItem} />}
+      </div>
     </div>
   );
 }
