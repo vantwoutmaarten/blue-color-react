175543862ba567d9acd978cbd4ca9c2c726b24f2
vantwoutmaarten <maarten@vantwout.net>
2023-09-19 21:15:01 +0000

    Deploying to gh-pages from @ vantwoutmaarten/blue-color-react@77e8ac31d9f3099c0cde84f0a82e2b38c6faa60d 🚀

--- _next/static/rExzvcXkjvWHfrgUTfY9L/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a11be5e4ae7ed75b.css","static/chunks/pages/index-2b00332f3e01237e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
