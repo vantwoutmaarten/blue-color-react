053112acd6083778d88a5444483587d9d1deac30
vantwoutmaarten <maarten@vantwout.net>
2023-09-20 06:35:24 +0000

    Deploying to gh-pages from @ vantwoutmaarten/blue-color-react@9b4a310c4be2e40d9ca67896f173a8c69a1eea4c 🚀

---
+++ _next/static/1G_0zxGue9KT7I2CY1EZK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/695-70f349ae14f5f2de.js","static/css/a11be5e4ae7ed75b.css","static/chunks/pages/index-19261bd8e1e4a1ba.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/x-51k9z9pCcGxSvE-rf4E/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/57-f864a13abc51f72a.js","static/css/a11be5e4ae7ed75b.css","static/chunks/pages/index-61d98826064bcc05.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
